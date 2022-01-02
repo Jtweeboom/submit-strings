cefb280285c5ec206a0b75450baa85950c516796
Jtweeboom <95744635+Jtweeboom@users.noreply.github.com>
2022-01-02 15:24:19 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,20 @@
+# Do not modify these lines
+__winc_id__ = '71dd124b4a6e4d268f5973db521394ee'
+__human_name__ = 'strings'
+
+# Add your code after this line
+
+scorer_name_0 = "Gullit"
+scorer_name_1 = "Van Basten"
+goal_0 = 32
+goal_1 = 54
+scorers = (scorer_name_0) + str (goal_0), (scorer_name_1) + str (goal_1)
+report = f"{scorer_name_0} scored in the {goal_0}nd minute.\n{scorer_name_1} scored in the {goal_1}th minute"
+print (report)
+player = "Erwin Koeman"
+first_name = player.find ("Erwin")
+print (first_name)
+last_name_len = player.find ("Koeman")
+print(last_name_len, len, int (last_name_len))
+name_short = "E Koeman"
+chant = "Erwin(!)"
